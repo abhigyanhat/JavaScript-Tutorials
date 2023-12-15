b65c55a5badae082f0b692555c8aea36b9085d10
Abhigyan Kumar <99490286+abhigyanhat@users.noreply.github.com>
2023-12-15 14:04:34 +0000

    datatypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -2,4 +2,10 @@
 
 // alert(3+3) // We are using nodejs, not brower 
 
-console.log(3+3); console.log("Abhigyan") 
+// console.log(3+3); console.log("Abhigyan") //
+let age = 5
+
+// console.log(typeof age)
+
+console.log(typeof null);  // null is a object
+console.log(typeof undefined);  // undefined
