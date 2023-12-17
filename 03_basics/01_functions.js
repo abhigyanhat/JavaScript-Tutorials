1846f9cbcf657b58f5c5d022ac3f44dcff123ecb
Abhigyan Kumar <99490286+abhigyanhat@users.noreply.github.com>
2023-12-17 13:25:30 +0000

    addition

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,32 @@
+function SayMyName() {
+    // console.log("A");
+    // console.log("B");
+    // console.log("H");
+    // console.log("I");
+    // console.log("G");
+    // console.log("Y");
+    // console.log("A");
+    // console.log("N");
+}
+
+SayMyName()
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1 + number2);
+// }
+// addTwoNumbers()
+
+function addTwoNumbers(number1, number2) {
+    // let result= number1+number2
+    // return result
+
+    return number1 + number2
+}
+
+const result= addTwoNumbers(3,4)
+// console.log(result);
+
+function UserLoggedIn(username) {
+    return `${username} Just logged In`
+}
+console.log(UserLoggedIn("Abhigyan"));
